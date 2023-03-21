ae5b9dd8441793d1a8404f32ee2a089af89a3b49
habib7042 <ahasanh303@gmail.com>
2023-03-21 14:24:42 +0600

    all update

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,5 +1,6 @@
 import styles from "../../styles/Home.module.css";
 import Head from "next/head";
+import { useState } from "react";
 export async function getStaticProps() {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await res.json();
@@ -11,7 +12,17 @@ export async function getStaticProps() {
   };
 }
 
-function blogs({ posts}) {
+function blogs({ posts }) {
+  const [currentPage, setCurrentpage] = useState(1);
+
+  function onNextHendler() {
+    setCurrentpage(currentPage + 1);
+  }
+  function onPevHendler() {
+    setCurrentpage(currentPage - 1);
+  }
+  const visiblePost = posts.slice((currentPage - 1) * 10, currentPage * 10);
+
   return (
     <>
       <Head>
@@ -19,12 +30,14 @@ function blogs({ posts}) {
         <meta name="keywords" content="magicweb blogs" />
       </Head>
       <h3 className={styles.title}>Blogs</h3>
-      {posts.map((post) => (
+      {visiblePost.map((post) => (
         <div key={post.id} className={styles.card}>
           <h3 className={styles.card_title}>{post.title}</h3>
           <p className={styles.card_text}>{post.body}</p>
         </div>
       ))}
+      <button disabled={currentPage === 1} onClick={onPevHendler}>Pev Page</button>
+      <button onClick={onNextHendler}>Next Page</button>
     </>
   );
 }
